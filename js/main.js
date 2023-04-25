8547392c2403d91d62bb7d9a22609d4fa46c2123
benjaminpaez <benjaapaez9@gmail.com>
2023-04-25 19:33:08 -0300

    Cambios de estilos, consumo de api

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$.ajax({
+		url: 'https://reqres.in/api/users?page=1',
+		dataType: 'json',
+		success: function(data) {
+		var html = '';
+		for(var i=0; i<3; i++) {
+			var user = data.data[i];
+			html += '<div class="col-lg-4">';
+			html += '<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">';
+		
+			html += '<rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>';
+			html += '<image x="0" y="0" width="100%" height="100%" xlink:href="' + user.avatar + '" />';
+			html += '</svg>';
+			html += '<h5 class="fw-bold mt-1">' + user.first_name + ' ' + user.last_name + '</h5>';
+			html += '<p>' + user.email + '</p>';
+			html += '</div>';
+		}
+		$('#personal .row').html(html);
+		}
+	});
+});
